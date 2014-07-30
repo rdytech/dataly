1b0b4d0ffeea3391ad330b29369a3356bc088e21
John D'Agostino <john.dagostino@gmail.com>
2014-07-30 13:37:59 +1000

    Ability to map to a method on the class (ala rails)
    This change is backwards incompatible as it changes the arguments
    to field to be a hash rather than an symbol or proc

--- lib/dataly/mapper.rb
+++ lib/dataly/mapper.rb
@@ -1,4 +1,5 @@
-require 'pry'
+require 'byebug'
+
 module Dataly
   class Mapper
     attr_reader :model
@@ -6,8 +7,8 @@ module Dataly
     class_attribute :fields
     self.fields = {}
 
-    def self.field(from, symbol_or_proc)
-      fields[from] = symbol_or_proc
+    def self.field(from, hash)
+      fields[from] = hash
     end
 
     def initialize(model)
@@ -16,23 +17,44 @@ module Dataly
 
     def process(row)
       row.map { |name, value|
-        name = switch(name)
-        value = transform(name, value)
+        name, value = switch(name, value)
         [name.to_sym, (value.blank? ? nil : value)] if attributes.include?(name.to_s)
       }.compact.to_h
     end
 
-    def switch(name)
-      mapping = fields.fetch(name, name)
-      mapping.respond_to?(:call) ? name : mapping
+    def switch(name, value)
+      key = name.to_sym
+      return [name, value] unless mapping_exists?(key)
+
+      name = map_to(key, name)
+      value = transform(key, value)
+
+      [name, value]
     end
 
     def transform(name, value)
-      if fields[name.to_sym].respond_to?(:call)
-        fields[name.to_sym].call(value)
+      transformer = map_value(name)
+      return value unless transformer
+
+      if transformer.respond_to?(:call)
+        transformer.call(value)
+      elsif respond_to?(transformer)
+        send(transformer, value)
       else
         value
       end
+    end
+
+    def map_value(key)
+      fields[key][:value]
+    end
+
+    def map_to(key, default)
+      fields[key][:to] || default
+    end
+
+    def mapping_exists?(key)
+      fields[key]
     end
 
     def attributes
