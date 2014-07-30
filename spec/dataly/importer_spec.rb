0743bc5cb218561a62ce01d47e21d6cf3366b0bd
Andrew McNamara <andrewm@jobready.com.au>
2014-07-30 18:23:57 +1000

    Add additional tests for reporting.

--- spec/dataly/importer_spec.rb
+++ spec/dataly/importer_spec.rb
@@ -4,12 +4,10 @@ class Sample
 end
 
 class SampleMapper < Dataly::Mapper
-
   def attributes
     %w(name status)
   end
 end
-
 
 class SampleImporter < Dataly::Importer
   model Sample
@@ -28,30 +26,51 @@ end
 
 describe Dataly::Importer do
   include FakeFS::SpecHelpers
-
-  let(:importer) { SampleImporter.new('sample.csv') }
+  let(:reporter) { instance_double(Dataly::Reporter) }
+  let(:importer) { SampleImporter.new('sample.csv', { reporter: reporter }) }
+  let(:sample_file) { double(:sample) }
 
   before do
     create_sample_csv('sample.csv')
-    allow(Sample).to receive(:new).and_return(double(:sample, save!: true))
+    allow(reporter).to receive(:processed)
+    allow(reporter).to receive(:report)
+    allow(Sample).to receive(:new).and_return(sample_file)
+    allow(sample_file).to receive(:save!).and_return(true)
   end
 
   it 'creates an instance of the importers model for each row' do
     expect(Sample).to receive(:new).with({ name: "Jack Bauer",
-                                           status: "active" })
+      status: "active" })
     importer.process
   end
+  context 'Default notification' do
+    it 'adds successful row from import to report' do
+      expect(reporter).to receive(:processed)
+      importer.process
+    end
 
+    it 'adds unsuccessful rows to report' do
+      allow(sample_file).to receive(:save!).and_raise(StandardError)
+      expect(reporter).to receive(:failed)
+      importer.process
+    end
+
+    it 'returns the report' do
+      allow(reporter).to receive(:report).and_return('report')
+      expect(importer.process).to eql('report')
+    end
+
+  end
   context 'Error notification to raise' do
     let(:importer) do
       SampleImporter.new('sample.csv',
-                         { errors: raise })
+        { errors: raise })
     end
     let(:sample) { Sample.new }
 
     before do
-      allow(sample).to receive(:save!).and_raise(StandardError)
-      allow(Sample).to receive(:new).and_return(sample)
+      allow(reporter).to receive(:failed)
+      allow(sample_file).to receive(:save!).and_raise(StandardError)
     end
 
     it 'raises an exception' do
